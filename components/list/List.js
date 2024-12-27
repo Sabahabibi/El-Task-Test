22d22205784a463a7dc1dc9f81ce9046f8e63cef
sia <sia@cfbtel.com>
2024-12-27 14:25:51 +0330

    mock data

--- components/list/List.js
+++ components/list/List.js
@@ -1,6 +1,21 @@
 import { El } from "../../utils/El.js";
 import { Card } from "../card/Cards.js";
 
+const phoneList = [
+  {
+    id: 1,
+    imageUrl: "https://picsum.photos/200/300",
+    name: "iphone",
+    price: 1000,
+  },
+  {
+    id: 2,
+    imageUrl: "https://picsum.photos/200/300",
+    name: "laptop",
+    price: 1000,
+  },
+];
+
 export function List() {
   return El({
     element: "div",
